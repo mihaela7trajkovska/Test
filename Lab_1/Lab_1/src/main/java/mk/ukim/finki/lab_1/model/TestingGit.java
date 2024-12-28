4a4cbfecf090f93a6c97c5248a9634ae8591a408
Mihaela Trajkovska <mihaelatrajkovska0gmail.com>
2024-12-28 13:30:34 +0100

    Another model class

---
+++ Lab_1/Lab_1/src/main/java/mk/ukim/finki/lab_1/model/TestingGit.java
@@ -0,0 +1,11 @@
+package mk.ukim.finki.lab_1.model;
+
+public class TestingGit {
+    String name;
+    String surname;
+
+    public TestingGit(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+}
